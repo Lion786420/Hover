cb44b3b313c10bffa37a09fc2b2e8e1d14666bf2
Lion <thapaaniket12345@gmail.com>
2023-01-11 14:05:15 +0545

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+const firstElement=document.querySelectorAll(".panel");
+firstElement.forEach((i)=>{
+i.addEventListener("mouseover",()=>
+i.classList.add("active")
+);
+i.addEventListener("mouseout",()=>
+i.classList.remove("active")
+);
+});
